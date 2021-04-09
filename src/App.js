cdb6d3021c99971fac0b4acae79e506e49e8ce57
Ghadeer <apps02977@gmail.com>
2021-04-09 19:24:12 +0300

    structure of files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Profile from "./Components/Home";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import NavBar from "./NavBar";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route path="/home/">
+          <home />
+        </Route>
+        <Redirect exact from="/logout" to="/login" />
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "./redux/actions";
+import Logout from "./components/Logout";
+
+const NavBar = ({ user, logout }) => (
+  <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        ARTIC
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link to="/home" className="nav-link">
+              home
+            </Link>
+          </li>
+          {user ? (
+            <>
+              <li onClick={() => logout()} className="nav-item">
+                {/* <a className="nav-link">
+                  logout
+                  </a> */}
+                <Link to="/login" className="nav-link">
+                  Logout
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/signup" className="nav-link">
+                  Signup
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  </>
+);
+
+const mapStateToProps = ({ user }) => ({ user });
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(logout()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
