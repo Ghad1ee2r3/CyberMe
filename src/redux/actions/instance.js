038e1872789dc61146e4d5a4ea7d5834ca09b9ef
Msk6 <Maha.s.kh@hotmail.com>
2021-04-18 19:59:58 +0300

    change instance

--- src/redux/actions/instance.js
+++ src/redux/actions/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
-});
+  baseURL: "http://mmrg98.pythonanywhere.com/", 
+}); 
+// http://127.0.0.1:8000/
 
 export default instance;
